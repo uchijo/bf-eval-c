f400987026876bccf71350b65dea54b5ae062ca5
Uchijo <kgtnttki.yuta@gmail.com>
2024-09-12 23:07:34 +0900

    とりあえず動く

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // enum for the instructions
 enum Instruction {
@@ -18,6 +20,50 @@ struct VirtualInstruction {
     int data;
 };
 
+int *cacheJumpDest(char *src) {
+    int src_length = strlen(src);
+	int *jumpDest = calloc(src_length, sizeof(int));
+	// for pc, c := range src {
+	for (int i=0; i<src_length; i++) {
+		if (src[i] == '[') {
+			int start = i;
+			int nest = 1;
+			for (;;) {
+				start++;
+				if (src[start] == '[') {
+					nest++;
+				} else if (src[start]== ']') {
+					nest--;
+					if (nest == 0) {
+						break;
+					}
+				} else {
+					continue;
+				}
+			}
+			jumpDest[i] = start;
+		} else if (src[i] == ']') {
+			int start = i;
+			int nest = 1;
+			for (;;) {
+				start--;
+				if (src[start] == ']') {
+					nest++;
+				} else if (src[start] == '[') {
+					nest--;
+					if (nest == 0) {
+						break;
+					}
+				} else {
+					continue;
+				}
+			}
+			jumpDest[i] = start;
+		}
+	}
+	return jumpDest;
+}
+
 int main() {
     // read file named m.bf
     FILE *file = fopen("m.bf", "r");
@@ -34,8 +80,61 @@ int main() {
         if (c == EOF) {
             break;
         }
+        if (c != '<' && c!='>' && c!='+' && c!='-' && c!='.' && c!=',' && c!='[' && c!=']') {
+            continue;
+        }
         buffer[i] = c;
         i++;
+    }
+    buffer[i] = '\0';
+
+    // printf("%s\n", buffer);
+
+    int pc = 0;
+    int pointer = 15000;
+    int mem[30000];
+    int instructions = strlen(buffer);
+    int *jump_dest = cacheJumpDest(buffer);
+    // for (int i=0; i<30000; i++) {
+    //     printf("mem %d: %d\n", i, mem[i]);
+    // }
+    // printf("mem %d: %d\n", pointer, mem[pointer]);
+    // printf("jump_dest for %d: %d\n", pc, jump_dest[pc]);
+    // return 0;
+    for (;;) {
+        // printf("current instr: %d, %c\n", pc, buffer[pc]);
+        // printf("pointer: %d, mem: %d\n", pointer, mem[pointer]);
+        if (pc >= instructions) {
+            break;
+        }
+        switch (buffer[pc]) {
+            case '>':
+                pointer++;
+                break;
+            case '<':
+                pointer--;
+                break;
+            case '+':
+                mem[pointer]++;
+                break;
+            case '-':
+                mem[pointer]--;
+                break;
+            case '.':
+                printf("%c", mem[pointer]);
+                break;
+            case '[':
+                if (mem[pointer] == 0) {
+                    pc = jump_dest[pc];
+                }
+                break;
+            case ']':
+                if (mem[pointer] != 0) {
+                    pc = jump_dest[pc];
+                }
+                break;
+        }
+        pc++;
     }
 }
 
